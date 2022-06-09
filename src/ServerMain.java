49087be76ca762b553e7348c9d4504f00a66cf6e
ChiaraMaggi <maggichiara99@gmail.com>
2022-06-09 17:53:23 +0200

    varie modifiche

--- src/ServerMain.java
+++ src/ServerMain.java
@@ -1,10 +1,6 @@
-import java.io.BufferedOutputStream;
-import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.net.BindException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
--- src/ServerRunnable.java
+++ src/ServerRunnable.java
@@ -3,7 +3,6 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
-import java.util.HashMap;
 
 public class ServerRunnable implements Runnable {
     Socket clientSocket;
